f570f8eb082b9246a69d5391656d981cb39d3974
Danny Nguyen <danny@80pct.com>
2022-01-29 16:59:12 -0500

    Add Problem 16

---
+++ p16.fsx
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S dotnet fsi
+
+#load "common.fsx"
+open Common
+
+(*
+    Power digit sum
+
+    Problem 16
+
+    2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+    What is the sum of the digits of the number 2**1000?
+*)
+
+string (pown 2I 1000)
+|> Seq.map (fun c -> string c |> int)
+|> Seq.sum
+|> pp

859e2bd5da4c0888df1f6c9a299bc94f4817fcf1
Danny Nguyen <danny@keeb.io>
2022-02-01 09:34:40 -0500

    Speed up search by doing early exits

--- p50.fsx
+++ p50.fsx
@@ -8,14 +8,13 @@ let primes = primesUpTo maxPrime
 
 let consectivePrimeSums n =
     primes
+    |> Seq.rev
     |> Seq.windowed n
     |> Seq.map Array.sum
-    |> Seq.filter isPrimeL
-    |> Seq.filter (fun p -> p <= maxPrime)
+    |> Seq.filter (fun s -> s <= maxPrime)
+    |> Seq.tryFind isPrimeL
 
-seq { 1000..-1..500 }
+seq { 1000..-1..2 }
 |> Seq.map (fun n -> (n, consectivePrimeSums n))
-|> Seq.filter (fun (n, s) -> not (Seq.isEmpty s))
-|> Seq.map (fun (n, s) -> (n, Seq.max s))
-|> Seq.maxBy (fun (n, s) -> n)
+|> Seq.tryFind (fun (n, s) -> s.IsSome)
 |> pp
